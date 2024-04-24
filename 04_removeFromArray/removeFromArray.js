2f071f129a9742daa410fb50eac74042c364986b
frockett <119839633+frockett@users.noreply.github.com>
2024-04-24 14:29:55 +0800

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(inputArray, ...toRemove) {
+    for (item of inputArray) {
+        for (element of toRemove) {
+            const index = inputArray.indexOf(element);
+            if (index != -1) {
+                const x = inputArray.splice(index, 1);
+                console.log(x)
+            }
+        }
+    }
+
+    return inputArray;
 
 };
 
