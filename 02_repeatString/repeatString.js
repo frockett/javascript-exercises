caf42f9cc3904ff0a9c3501728603b47f49c05d4
frockett <119839633+frockett@users.noreply.github.com>
2024-04-24 11:29:48 +0800

    finished 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
 
+    if (repeats < 0) {
+        return "ERROR";
+    }
+
+    let output = "";
+    
+    for (i = 0; i < repeats; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const a = Array.from(input);
+    let output = ""
+    for (i = a.length - 1; i > -1; i--) {
+        output += a[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
