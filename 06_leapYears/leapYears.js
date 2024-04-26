a9be5f34a5cb6fb35d412ceb72fac64207174bdd
frockett <119839633+frockett@users.noreply.github.com>
2024-04-26 14:36:44 +0800

    finished 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(input) {
+
+    if (input % 4 === 0 && (input % 100 !== 0 || input % 400 === 0)) {
+        return true;
+    }
+    else return false;
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
