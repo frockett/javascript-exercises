5b2bd3f00110dac445fbce90d25386c318864c41
frockett <119839633+frockett@users.noreply.github.com>
2024-04-26 14:29:57 +0800

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let total = 0;
 
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR";
+    }
+
+    const floor = x < y ? x : y;
+    const ceiling = x > y? x : y;
+
+    for (i = floor; i < ceiling + 1; i++) {
+        total += i;
+    }
+
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
