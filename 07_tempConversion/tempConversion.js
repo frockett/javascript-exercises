8f3e0f17cfab4dac06df6c6a65964abdfe2a9a5c
frockett <119839633+frockett@users.noreply.github.com>
2024-04-26 14:43:08 +0800

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let celsius = ((input - 32) * 5/9);
+  const roundResult = Math.round(celsius * 10) / 10;
+  return roundResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let fResult = ((input * 9/5) + 32)
+  const roundResult = Math.round(fResult * 10) / 10;
+  return roundResult;
 };
 
 // Do not edit below this line
